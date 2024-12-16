7846e45b9276823a1bac52273e01ffcfffd3fe41
AlexeyIgoshkin <russubcinema@gmail.com>
2024-12-16 12:44:28 +0500

    Коммит 4-го ДЗ (10 урок) Работа над ошибками

--- src/main/java/lesson_10/GEOMETRIC/GeometricInterface.java
+++ src/main/java/lesson_10/GEOMETRIC/GeometricInterface.java
@@ -1,13 +1,11 @@
 package lesson_10.GEOMETRIC;
 
 public interface GeometricInterface {
-    // Объединил методы по математическим вычислениям и цветам. Менее гибко, чем отдельно, но принцип интерфейса понятен
     void getPerimeter();
     void getArea();
     void setFillColor();
     void setBorderColor();
 
-    // Добавил те же вычисления через дефолтный метод квадрату
     default void fillColor(String fillColor) {
         System.out.println("Цвет заливки фигуры: " + fillColor);
     }
