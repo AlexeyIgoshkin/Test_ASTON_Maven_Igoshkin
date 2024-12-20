d0352779c5b02fb70de18dbf49ef84f1e79f32fb
AlexeyIgoshkin <russubcinema@gmail.com>
2024-12-20 18:25:22 +0500

    Коммит 5-го ДЗ (12 урок, исправленный)

---
+++ src/main/java/lesson_12/ArrayValidation.java
@@ -0,0 +1,34 @@
+package lesson_12;
+
+import java.util.Arrays;
+
+public class ArrayValidation {
+    // Создаем метод с проверками
+    public static void arrayException(String[][] array) throws MyArraySizeException, MyArrayDataException {
+
+        //Убеждаемся, что массив имеет 4 строки, иначе - выкидываем ошибку размера.
+        if (array.length != 4) {
+            throw new MyArraySizeException("Массив не обработан. Допустимый размер массива: 4х4. Вы пытаетесь создать " + array.length + "строк");
+        }
+
+        //Убеждаемся, что строка имеет 4 индекса, иначе - выкидываем ошибку размера.
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Массив не обработан. Допустимый размер массива: 4х4. Массив " + i + " содержит " + array[i].length + " элементов");
+            }
+        }
+
+        //Убеждаемся, что в двумерном массиве нет значений, которые не могут быть преобразованы в int, иначе - выкидываем ошибку данных.
+        int summ = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    summ += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Массив не обработан. Неправильные данные в ячейке " + i + "-" + j + ": " + array[i][j]);
+                }
+            }
+        }
+        System.out.println("Сумма чисел массива: " + summ);
+    }
+}
