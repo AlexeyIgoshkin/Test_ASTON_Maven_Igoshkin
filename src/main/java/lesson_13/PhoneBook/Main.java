55b46d1bc6d693024273c764a4a13392e26d3e17
AlexeyIgoshkin <russubcinema@gmail.com>
2024-12-21 21:56:37 +0500

    Коммит 6-го ДЗ (13 урок)

--- src/main/java/lesson_13/PhoneBook/Main.java
+++ src/main/java/lesson_13/PhoneBook/Main.java
@@ -1,36 +1,13 @@
-package lesson_13;
+package lesson_13.PhoneBook;
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String[] array = {
-                "Красный","Белый", "Желтый", "Оранжевый", "Черный",
-                "Зеленый", "Синий", "Оранжевый", "Серебряный", "Фиолетовый",
-                "Желтый", "Серый", "Голубой", "Оранжевый", "Коричневый"
-        };
-
-        System.out.println("Изначальный список из массива: \n" + Arrays.toString(array));
-
-        LinkedHashSet<String> set = new LinkedHashSet<>(Arrays.asList(array));
-
-        System.out.println();
-
-        System.out.println("Преобразованный список в хэшсет с удаленными дубликатами в порядке добавления: \n" + set);
-        System.out.println();
-
-        System.out.println("Хэшмап для подсчета количества слов в списке (без учета регистра):");
-        Map<String, Integer> wordCount = new HashMap<>();
-        for (String word : array) {
-            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
-        }
-
-        wordCount.forEach((word, count) -> System.out.println(word + ": " + count));
-
-        System.out.println();
-
+        //Экземпляр справочника
         PhoneBook phoneBook = new PhoneBook();
 
+        //Добавляем данные через метод .add()
         phoneBook.add("Александр", "Васильев", "+79991234567");
         phoneBook.add("Мария", "Беляева", "+79234567890"); // Фамилия повторяется 3 раза
         phoneBook.add("Екатерина", "Федорова", "+79012345678");
@@ -43,12 +20,16 @@ public class Main {
         phoneBook.add("Татьяна", "Беляева", "+79993334455"); // Фамилия повторяется 3 раза
         phoneBook.add("Виктор", "Павлов", "+79880009999");
         phoneBook.add("Алексей", "Морозов", "+79623424242"); // Добавляем Алексею Морозову еще один номер. Фамилия повторяется 3 раза
-        phoneBook.get("Павлов");
+        phoneBook.add("Лариса", "Беляева", "+79005515165"); // Добавляем еще одну Беляеву в список. Фамилия повторяется 4 раза
 
+
+        // Получаем данные через метод .get() так
         List<String> telephones1 = phoneBook.get("Морозов");
         List<String> telephones2 = phoneBook.get("Беляева");
         System.out.println("Телефоны по искомой фамилии: " + telephones1);
         System.out.println("Телефоны по искомой фамилии: " + telephones2);
 
+        // Или так
+        System.out.println("Телефоны по искомой фамилии: " + phoneBook.get("Павлов"));
     }
 }
